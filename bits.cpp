1b8a3f8d926416fdaeb09aac88e45afa640b054f
renardal <arenardvv@gmail.com>
2017-07-19 11:14:22 +0900

    Update bits.cpp

--- bits.cpp
+++ bits.cpp
@@ -1,12 +1,6 @@
 /*
 Lucky number: look for the next number such that sum of first 3 digits = sum of last 3 digits
 */
-
-
-#include <bits/stdc++.h>
-
-using namespace std;
-
 bool lucky(int y){
     vector<int> d;
     for(int i=0; i<6; i++){
@@ -16,18 +10,10 @@ bool lucky(int y){
     
     return (d[0]+d[1]+d[2]) == (d[3]+d[4]+d[5]);
 }
-
 int onceInATram(int& x) {
     // Complete this function  
     x++;
     while( !lucky(x) ) x++;
     
     return x;
-}
-
-int main() {
-    int x;
-    cin >> x;
-    cout << onceInATram(x) << endl;
-    return 0;
 }
